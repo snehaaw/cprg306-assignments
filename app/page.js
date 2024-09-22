a1748698f0557aad9159394a24d5522659ec2320
snehaaw <138998720+snehaaw@users.noreply.github.com>
2024-09-22 13:26:04 -0600

    week-2 assignment done
    did week-2 assignment (created page.js, student-info.js and wrote the code)

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
-import Image from "next/image";
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <main>
-      
+      <h1>CPRG 306: Web Development 2 - Assignments</h1>
+      <Link href="/week-2">Go to Week 2 Page</Link>
     </main>
   );
 }
+
---
+++ app/week-2/student-info.js
@@ -0,0 +1,14 @@
+
+
+import Link from 'next/link';
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <p>Sneha</p>
+      <Link href="https://github.com/snehaaw/cprg306-assignments">
+        https://github.com
+      </Link>
+    </div>
+  );
+}
