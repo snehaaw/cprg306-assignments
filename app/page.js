c293d1028ee1a6bfba9fd55886b3efed942d37a2
snehaaw <138998720+snehaaw@users.noreply.github.com>
2024-09-30 20:58:59 -0600

    did week3 assignment
    added item,item-list and page.js in week-3 folder  and changed the title in layout.js to shopping list

--- app/page.js
+++ app/page.js
@@ -4,7 +4,9 @@ export default function Home() {
   return (
     <main>
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
-      <Link href="/week-2">Go to Week 2 Page</Link>
+      <Link href="/week-2" className="text-blue-600 hover:underline">Week 2 Assignment</Link> <br/>
+      <Link href="/week-3" className="text-blue-600 hover:underline">Week 3 Assignment</Link>
+
     </main>
   );
 }
